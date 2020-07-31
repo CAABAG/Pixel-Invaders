87b147e24922cf6d04c3266e9bf8892ce3c5417f
Prezes <piotrbrzostowski96@gmail.com>
2020-07-31 15:35:09 +0200

    Update main to test player class

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include "WindowManager.h"
+#include "Player.h"
+#include "Bullet.h"
+#include <vector>
 
 int main()
 {
 	WindowManager::Create(800, 680);
+
+	sf::Texture playerTexture;
+	playerTexture.loadFromFile("../graphics/player.png");
+	Player player(playerTexture, 50.f, 90.f);
+
+	std::vector<Bullet> bullets;
 
 	while (WindowManager::IsWindowOpen())
 	{
@@ -16,7 +25,12 @@ int main()
 			}
 		}
 
+		player.Update(bullets);
+
 		WindowManager::ClearWindow();
+
+		player.Render();
+
 		WindowManager::DisplayWindow();
 	}
 
