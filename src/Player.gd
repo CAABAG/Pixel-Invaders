50b627854a4888a3a97df6880ee18c472821ac27
CAABAG <piotrbrzostowski96@gmail.com>
2021-10-03 19:23:25 +0200

    Fix getting height of node before it's initialized

--- src/Player.gd
+++ src/Player.gd
@@ -4,7 +4,7 @@ var screen_size
 var movement = 0
 var speed = 90
 var shot_timestamp = 0
-var half_height = get_node("AnimatedSprite").get_sprite_frames().get_frame($AnimatedSprite.animation,0).get_size().y/2
+var half_height
 export (PackedScene) var Bullet
 
 enum Direction {RIGHT, LEFT}
@@ -13,6 +13,7 @@ func _ready():
 	screen_size = get_viewport_rect().size
 	position.x = screen_size.x/2
 	position.y = screen_size.y - screen_size.y/9
+	half_height = get_node("AnimatedSprite").get_sprite_frames().get_frame($AnimatedSprite.animation,0).get_size().y/2
 
 func acceleration(factor):
 	return speed * factor
