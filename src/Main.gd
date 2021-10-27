50e717144b5c8f9e494787e82b53a10d7ca831d0
CAABAG <piotrbrzostowski96@gmail.com>
2021-10-27 21:26:23 +0200

    Clean main script

--- src/Main.gd
+++ src/Main.gd
@@ -2,21 +2,11 @@ extends Node2D
 
 export (PackedScene) var Enemy
 
-var aliens = []
-
-var main_alien
+var screen_size
 
 func _ready():
-	for i in 30:
-		var alien = Enemy.instance()
-		add_child(alien)
-		aliens.append(alien)
-	for i in range(0,10):
-		aliens[i].position.x = 57 * (i+1)
-		aliens[i].position.y = 100
-	for i in range(10,20):
-		aliens[i].position.x = 57 * ((i+1)-10) + 25
-		aliens[i].position.y = 150
-	for i in range(20,30):
-		aliens[i].position.x = 57 * ((i+1)-20)
-		aliens[i].position.y = 200
+	screen_size = get_viewport_rect().size
+	var alien = Enemy.instance()
+	alien.position.x = screen_size.x/2
+	alien.position.y = screen_size.y/5
+	add_child(alien)
