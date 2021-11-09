40cd48978f6597d158d2cb58715a0860478e02a5
CAABAG <piotrbrzostowski96@gmail.com>
2021-11-09 21:35:40 +0100

    Add enemy swinging

--- src/Enemy.gd
+++ src/Enemy.gd
@@ -1,4 +1,40 @@
 extends Area2D
 
+const speed = 80
+const max_speed = 160
+var direction = -1 # -1 = left, 1 = right
+var movement = 0
+
+var min_x_pos
+var max_x_pos
+
+func _ready():
+	position.x = (min_x_pos + max_x_pos)/2
+
+func acceleration(factor):
+	return speed * factor
+
+func swing():
+	if movement > max_speed:
+		movement = max_speed
+	if movement < -max_speed:
+		movement = -max_speed
+	match direction:
+		-1:
+			if get_global_position().x > min_x_pos:
+				movement -= acceleration(.1)
+				return
+			direction = 1
+		1:
+			if get_global_position().x < max_x_pos:
+				movement += acceleration(.1)
+				return
+			direction = -1
+
+func _process(delta):
+	swing()
+	position.x += movement * delta
+	$AnimatedSprite.animation = "default"
+
 func hit():
 	queue_free()
