782e5f317544a1ee4a7133aadb355c93cd249e3c
Piotr Brzostowski <46322827+CAABAG@users.noreply.github.com>
2021-11-09 20:49:57 +0100

    Add enemy dying (#10)
    * Added enemy dying
    
    * Fix whitespace
    
    * Revert change for different branch
    
    * Update project settings
    
    * Rm obsolete Bullet include in Enemy

--- src/Bullet.gd
+++ src/Bullet.gd
@@ -17,3 +17,8 @@ func _physics_process(delta):
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+func _on_Bullet_area_entered(area):
+	if area.is_in_group("aliens"):
+		if is_going_up == true:
+			area.hit()
