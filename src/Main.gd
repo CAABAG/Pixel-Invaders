d81a4388cdce115865aaab26e99db65abe422b5c
Piotr Brzostowski <46322827+CAABAG@users.noreply.github.com>
2021-11-10 20:40:30 +0100

    Add enemy swinging (#11)
    * Add enemy swinging
    
    * Revert Enemy collision shape radius change
    
    * Increase acceleration speed on the enemies

--- src/Main.gd
+++ src/Main.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 export (PackedScene) var Enemy
 
+var swing_range = 30
 var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	var alien = Enemy.instance()
-	alien.position.x = screen_size.x/2
+	alien.min_x_pos = screen_size.x/2 - swing_range
+	alien.max_x_pos = screen_size.x/2 + swing_range
 	alien.position.y = screen_size.y/5
 	add_child(alien)
 	alien.add_to_group("aliens")
