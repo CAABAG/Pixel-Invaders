bf4d84e3ceeade4b6e9318df1c40666e54d8b090
CAABAG <piotrbrzostowski96@gmail.com>
2021-11-16 18:40:51 +0100

    Add enemy shooting

--- src/Enemy.gd
+++ src/Enemy.gd
@@ -1,15 +1,35 @@
 extends Area2D
 
+var Bullet = preload("res://res/scenes/Bullet.tscn")
+
 const speed = 80
 const max_speed = 160
+const min_shooting_interval = 1
+const max_shooting_interval = 3
 var direction = -1 # -1 = left, 1 = right
 var movement = 0
 
+var shot_timer = Timer.new()
 var min_x_pos
 var max_x_pos
 
 func _ready():
 	position.x = (min_x_pos + max_x_pos)/2
+	add_child(shot_timer)
+	shot_timer.connect("timeout", self, "_shoot")
+	randomize()
+	shot_timer.one_shot = true
+	shot_timer.wait_time = rand_range(min_shooting_interval, max_shooting_interval)
+	shot_timer.start()
+
+func _shoot():
+	var bullet = Bullet.instance()
+	bullet.position.x = position.x
+	bullet.position.y = position.y
+	bullet.is_going_up = false
+	get_tree().get_root().add_child(bullet)
+	shot_timer.wait_time = rand_range(min_shooting_interval, max_shooting_interval)
+	shot_timer.start()
 
 func acceleration(factor):
 	return speed * factor
