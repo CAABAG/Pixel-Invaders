acb94164fbb3b97c054a7bd63d135ee0c2437945
CAABAG <piotrbrzostowski96@gmail.com>
2022-01-08 10:07:37 +0100

    Add two levels and their handling

--- src/Main.gd
+++ src/Main.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
-export (PackedScene) var Enemy
-
 var score = 0
-var screen_size
+var level_one = preload("res://res/scenes/LevelOne.tscn")
+var level_two = preload("res://res/scenes/LevelTwo.tscn")
+var current_scene
 
 func _ready():
-	screen_size = get_viewport_rect().size
-	var alien = Enemy.instance()
-	alien.position.y = screen_size.y/5
-	add_child(alien)
-	alien.add_to_group("aliens")
+	add_child(level_one.instance())
+	current_scene = "level_one"
+
+func _process(_delta):
+	for child in get_children():
+		if not is_instance_valid(child):
+			remove_child(child)
+
+func swap_levels():
+	if current_scene == "level_one":
+		add_child(level_two.instance())
+		current_scene = "level_two"
+	else:
+		add_child(level_one.instance())
+		current_scene = "level_one"
